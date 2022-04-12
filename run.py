f335af6fab641ed91670ece2ae82383c32b36648
JessMair <jessiegrewal11@gmail.com>
2022-04-12 12:27:42 +0000

    Add introduction and syntax for player to enter name.

---
+++ run.py
@@ -0,0 +1,8 @@
+# Intro for the player 
+
+print("You have opened the doors of the unknown")
+
+#Player prompted to enter name to begin playing. 
+
+name = input("Enter your name to play: ").capitalize()
+print ("Hello", name, "you have now entered the world of Adventure!!")
